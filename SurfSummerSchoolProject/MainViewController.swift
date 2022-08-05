f6ec839bfbbf8cb4e38a1ad3ea4bc1402380f624
sliamand <sliamand@mail.ru>
2022-08-05 19:58:59 +0300

    add navi as rootViewController

---
+++ SurfSummerSchoolProject/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Алексей Грачев on 05.08.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+}
